22569db0e164e6799ee0842618c0ca2b7b5371a2
Sidddanth Katipally <siddanth6365@gmail.com>
2023-06-23 13:03:41 +0530

    added proxy route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,9 +4,13 @@ const AirlineRoutes = require("./Airline-routes")
 const CityRoutes = require("./City-routes")
 const AirportRoutes = require("./Airport-routes")
 const FlightRoutes = require("./flight-routes")
+const { InfoController } = require("../../controllers");
+
 
 
 const router = express.Router();
+
+router.get("/info",InfoController.info)
 
 router.use("/airline", AirlineRoutes);
 router.use("/city", CityRoutes);
